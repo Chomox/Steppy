9c0ab5b00658649e1152d6d020566ee249c7b35e
Moto <mott.eng0124@gmail.com>
2020-08-31 22:42:04 +0900

    create Steppy Project

---
+++ Steppy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Steppy
+//
+//  Created by Motoyuki Ito on 8/31/20.
+//  Copyright © 2020 Moto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
