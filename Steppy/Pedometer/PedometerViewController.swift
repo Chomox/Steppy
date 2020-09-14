a0f55178a32f6ddfe789758cc4ae969020092fd7
Moto <mott.eng0124@gmail.com>
2020-09-15 01:39:10 +0900

    Introduce RxSwift  RxSwift

--- Steppy/Pedometer/PedometerViewController.swift
+++ Steppy/Pedometer/PedometerViewController.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 Moto. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class PedometerViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
